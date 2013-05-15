eddaeff5efbe3495376d8caac393e6434cfa408c
jamaal.taylor <j.jerrod.taylor@gmail.com>
2013-05-15 19:36:48 -0400

    initial commit

---
+++ src/main/java/edu/mason/insf/utils/Pattern.java
@@ -0,0 +1,11 @@
+package main.java.edu.mason.insf.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jamaaltaylor
+ * Date: 5/15/13
+ * Time: 7:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Pattern {
+}
