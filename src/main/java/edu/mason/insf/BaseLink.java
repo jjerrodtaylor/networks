9464ec2828abb089e501fc9954d744ec7cbf1013
jamaal.taylor <j.jerrod.taylor@gmail.com>
2013-05-15 23:23:09 -0400

    Removed the default files made by maven.
    I started implementing the virtual class for the base node.

---
+++ src/main/java/edu/mason/insf/BaseLink.java
@@ -0,0 +1,11 @@
+package main.java.edu.mason.insf;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jamaaltaylor
+ * Date: 5/15/13
+ * Time: 8:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaseLink {
+}
---
+++ src/test/java/edu/mason/insf/TestPattern.java
@@ -0,0 +1,11 @@
+package test.java.edu.mason.insf;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jamaaltaylor
+ * Date: 5/15/13
+ * Time: 10:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestPattern {
+}
