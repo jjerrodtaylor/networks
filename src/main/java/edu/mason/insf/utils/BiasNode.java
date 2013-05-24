0ea092b6d71c6d657b8cd293cfe28db9024adb80
jamaal.taylor <j.jerrod.taylor@gmail.com>
2013-05-24 08:32:12 -0400

    Added simple InputNode class. Starting on BiasNode for Adeline.

---
+++ src/main/java/edu/mason/insf/utils/BiasNode.java
@@ -0,0 +1,12 @@
+package edu.mason.insf.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jamaaltaylor
+ * Date: 5/24/13
+ * Time: 8:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BiasNode extends InputNode {
+
+}
---
+++ src/main/java/edu/mason/insf/utils/InputNode.java
@@ -0,0 +1,27 @@
+package edu.mason.insf.utils;
+
+import edu.mason.insf.BaseNode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jamaaltaylor
+ * Date: 5/24/13
+ * Time: 8:18 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class InputNode extends BaseNode {
+
+    public InputNode()
+    {
+        super();
+    }
+
+    public InputNode(double value, double error)
+    {
+        super();
+        this.value = value;
+        this.error = error;
+
+    }
+
+}
