af126890505ebdb54b6d2995abc1d2eb6221127c
jamaal.taylor <j.jerrod.taylor@gmail.com>
2013-06-08 12:13:45 -0400

    started adding test to actually test the adaline network.

--- src/test/java/edu/mason/insf/TestAdaline.java
+++ src/test/java/edu/mason/insf/TestAdaline.java
@@ -1,5 +1,12 @@
 package edu.mason.insf;
 
+import edu.mason.insf.ann.utils.Helper;
+import edu.mason.insf.ann.utils.Pattern;
+import org.junit.Test;
+import java.util.LinkedList;
+import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: jamaaltaylor
@@ -8,4 +15,11 @@ package edu.mason.insf;
  * To change this template use File | Settings | File Templates.
  */
 public class TestAdaline {
+
+    @Test
+    public void testTranformData()
+    {
+        ArrayList<String> testData = Helper.readFileToMemory("/Users/jamaaltaylor/Documents/workspace/networks/src/test/resources/breastcancer.csv");
+        ArrayList<Pattern<Integer>> inputPatterns = Helper.turnListToPattern(testData);
+    }
 }
