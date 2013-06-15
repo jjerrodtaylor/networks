939379e8c46ef97157114dd4c0acfc9704018d42
jamaal.taylor <j.jerrod.taylor@gmail.com>
2013-06-15 14:52:38 -0400

    Using HashMap instead of list. Implements IFeedForward interface.

--- src/main/java/edu/mason/insf/ann/adaline/AdalineNode.java
+++ src/main/java/edu/mason/insf/ann/adaline/AdalineNode.java
@@ -1,25 +1,25 @@
 package edu.mason.insf.ann.adaline;
 
 import edu.mason.insf.ann.BaseLink;
-import edu.mason.insf.ann.FeedForwardNode;
+import edu.mason.insf.ann.BaseNode;
 import edu.mason.insf.ann.IFeedForward;
 import edu.mason.insf.ann.utils.Constants;
 
 /**
  * The class that implements the adeline node model
  */
-public class AdalineNode extends FeedForwardNode implements IFeedForward
+public class AdalineNode extends BaseNode implements IFeedForward
 {
 
     public AdalineNode(double learningRate)
     {
-        value.set(Constants.LEARNING_RATE, learningRate);
+        value.put(Constants.LEARNING_RATE,learningRate);
     }
 
     public void learn()
     {
         double nodeError = value.get(Constants.NODE_VALUE)* -2.0;
-        error.set(Constants.NODE_ERROR, nodeError);
+        error.put(Constants.NODE_ERROR, nodeError);
         BaseLink link;
         int count = inLinks.size();
         double delta;
@@ -29,6 +29,19 @@ public class AdalineNode extends FeedForwardNode implements IFeedForward
             delta = value.get(Constants.LEARNING_RATE) * inLinks.get(i).getInValue(Constants.WEIGHT);
             inLinks.get(i).updateWeight(delta);
         }
+    }
+
+    public void run()
+    {
+        double total = 0;
+        int count = inLinks.size();
+
+        for(int i = 0; i< count; i++)
+        {
+            total += inLinks.get(i).weightedInValue(i);
+        }
+
+        this.setValue(Constants.NODE_VALUE, this.transferFunction(total));
     }
 
     public double transferFunction(double value)
