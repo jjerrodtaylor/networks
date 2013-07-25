a58365809c7f3bf6ee645a4874c4ff6d5076bb95
jamaal.taylor <j.jerrod.taylor@gmail.com>
2013-07-25 14:27:34 -0400

    A lot of things done to help with debugging.

---
+++ src/main/java/edu/mason/insf/ann/adaline/AdalineNetwork.java
@@ -0,0 +1,16 @@
+package edu.mason.insf.ann.adaline;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jamaaltaylor
+ * Date: 7/25/13
+ * Time: 2:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AdalineNetwork {
+
+    public AdalineNetwork(Integer numOfNodesInInput, Double learningRate)
+    {
+
+    }
+}
