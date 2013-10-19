6eeb1d1fbb8f00507b7520217b317e097996ab63
jamaal.taylor <j.jerrod.taylor@gmail.com>
2013-10-19 16:28:07 -0300

    Added SetUp function to adaline test

--- NetworksClasses/src/test/java/edu/mason/insf/AdalineTest.java
+++ NetworksClasses/src/test/java/edu/mason/insf/AdalineTest.java
@@ -5,6 +5,7 @@ import edu.mason.insf.ann.adaline.AdalineNode;
 import edu.mason.insf.ann.utils.Helper;
 import edu.mason.insf.ann.utils.Pattern;
 import edu.mason.insf.ann.utils.Constants;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -20,19 +21,26 @@ public class AdalineTest {
     ArrayList<Pattern<Double>> inputPatterns = helper.turnListToPattern(testData);
     ArrayList<Pattern<Double>> trainingData = helper.partitionDataSet(.1, inputPatterns);
 
-    @Test
-    public void testSetData()
+    @Before
+    public void SetUp()
     {
         aNetwork.setTrainingData(trainingData);
         aNetwork.setAdalineNode(aNode);
+        aNetwork.initializeNodes(10);
+        aNetwork.initializeLinks(11);
+        aNetwork.connectBiasNode();
+        aNetwork.connectNodesAndLinks(aNode);
+    }
+
+    @Test
+    public void testSetData()
+    {
         assertNotNull(aNetwork.getTrainingData());
     }
 
     @Test
     public void testInitializeNodes()
     {
-        //test initializing the nodes
-        aNetwork.initializeNodes(10);
         assertNotNull(aNetwork.getNodeList().get(0));
         assertNotNull(aNetwork.getNodeList().get(1));
         assertNotNull(aNetwork.getNodeList().get(2));
@@ -41,8 +49,6 @@ public class AdalineTest {
     @Test
     public void testInitializeLinks()
     {
-        //test initializing the links
-        aNetwork.initializeLinks(11);
         assertNotNull(aNetwork.getLinkList().get(0));
         assertNotNull(aNetwork.getLinkList().get(1));
         assertNotNull(aNetwork.getLinkList().get(2));
@@ -51,10 +57,6 @@ public class AdalineTest {
     @Test
     public void testConnectNodesAndLinks()
     {
-        aNetwork.initializeNodes(10);
-        aNetwork.initializeLinks(10);
-        aNetwork.connectBiasNode();
-        aNetwork.connectNodesAndLinks(aNode);
         assertEquals(aNetwork.getBiasNode(),aNetwork.getBiasNode().getInLinks().get(0).getOutNode());
         assertEquals(aNetwork.getNode(0), aNetwork.getLinkList().get(0).getOutNode());
         assertEquals(aNetwork.getNode(1), aNetwork.getLinkList().get(1).getOutNode());
