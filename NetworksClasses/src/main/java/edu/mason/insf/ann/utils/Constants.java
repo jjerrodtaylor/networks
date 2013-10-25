6c7503ceb1424204d8611675ba4cd0a3c0f7f1dc
jamaal.taylor <j.jerrod.taylor@gmail.com>
2013-10-25 17:07:53 -0300

    Added a few more constants
    Added constant for back propigation network as well as constants for
    the test file that I generated.

--- NetworksClasses/src/main/java/edu/mason/insf/ann/utils/Constants.java
+++ NetworksClasses/src/main/java/edu/mason/insf/ann/utils/Constants.java
@@ -11,5 +11,8 @@ public class Constants {
     public static int NODE_VALUE = 3;
     public static int MODE = 4;
     public static int INPUT_DATA = 5;
+    public static int MOMENTUM = 6;
     public static String FILE_NAME = "/Users/jamaaltaylor/Documents/workspace/networks/NetworksClasses/src/test/resources/breastcancer.csv";
+    public static String TEST_FILE = "/Users/jamaaltaylor/Documents/workspace/networks/NetworksClasses/src/test/resources/testfile.txt";
+    public static String DESKTOP_PATH = "/Users/jamaaltaylor/Desktop";
 }
