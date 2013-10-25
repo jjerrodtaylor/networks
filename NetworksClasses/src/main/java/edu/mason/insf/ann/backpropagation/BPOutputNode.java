50f6241a9e18a73a097e771516283d6bbef1e404
jamaal.taylor <j.jerrod.taylor@gmail.com>
2013-10-25 17:08:15 -0300

    Started on Back Propigation Stuff

--- NetworksClasses/src/main/java/edu/mason/insf/ann/backpropagation/BPOutputNode.java
+++ NetworksClasses/src/main/java/edu/mason/insf/ann/backpropagation/BPOutputNode.java
@@ -1,5 +1,31 @@
 package edu.mason.insf.ann.backpropagation;
 
+import edu.mason.insf.ann.utils.Constants;
 
-public class BPOutputNode {
+public class BPOutputNode extends BPNode {
+
+    public BPOutputNode(double learnRate, double momentum, int vSize, int eSize)
+    {
+        super();
+        value.put(Constants.LEARNING_RATE,learnRate);
+        value.put(Constants.MOMENTUM,momentum);
+    }
+
+    public double computerError()
+    {
+        double error = value.get(Constants.NODE_VALUE)*
+                       (1.0-value.get(Constants.NODE_VALUE))*
+                       (value.get(Constants.NODE_ERROR)-value.get(Constants.NODE_VALUE));
+        return 0.0;
+    }
+
+    /*
+    * I need to read over the learning algorithm more
+    * */
+    public void learn()
+    {
+
+    }
+
+
 }
