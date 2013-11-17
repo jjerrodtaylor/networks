02873778cf15b45711a9ad189820c59095da117d
jamaal.taylor <j.jerrod.taylor@gmail.com>
2013-11-17 12:13:47 -0300

    Additions made to the pattern class as well as the pattern test class. Updates were made to the JavaDoc for documentation.

--- NetworksClasses/src/main/java/edu/mason/insf/ann/utils/Pattern.java
+++ NetworksClasses/src/main/java/edu/mason/insf/ann/utils/Pattern.java
@@ -3,17 +3,26 @@ package edu.mason.insf.ann.utils;
 import java.util.LinkedList;
 
 /**
+ *  The formalized pattern set.
+ *  Pattern Set: S = {I,O}
+ *  where I is a set of input patterns and O is a set of corresponding desired output patterns.
  *
+ *  Input Set: I = {P k,j}
+ *  where k is the input pattern number and j is the input pattern component.
+ *
+ *  Output Set: O = {O k,j}
+ *  where k is the desired output pattern number and j is the desired output pattern component.
+ *
+ *  I use Generics so that your Pattern objects can hold any type.
  * @param <T>
  */
 public class Pattern<T extends Comparable<T>> {
 
-    public int id;
     public LinkedList<T> inputSet;
     public LinkedList<T> outputSet;
 
     /**
-     *
+     * Constructor
      */
     public Pattern()
     {
@@ -23,47 +32,31 @@ public class Pattern<T extends Comparable<T>> {
     }
 
     /**
-     *
-     * @return
-     */
-    public int getId() {
-        return id;
-    }
-
-    /**
-     *
-     * @param id
-     */
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    /**
-     *
-     * @return
+     * Returns the number of elements in the input pattern set.
+     * @return int
      */
     public int getInputPatternSize() {
         return inputSet.size();
     }
 
     /**
-     *
-     * @return
+     * Returns the number of elements in the output pattern set.
+     * @return int
      */
     public int getOutputPatternSize() {
         return outputSet.size();
     }
 
     /**
-     *
-     * @return
+     * Returns the input pattern set.
+     * @return LinkedList<T>
      */
     public LinkedList<T> getInputSet() {
         return inputSet;
     }
 
     /**
-     *
+     * Set the input pattern set.
      * @param inputSet
      */
     public void setInputSet(LinkedList<T> inputSet) {
@@ -71,15 +64,15 @@ public class Pattern<T extends Comparable<T>> {
     }
 
     /**
-     *
-     * @return
+     * Returns the output pattern set.
+     * @return LinkedList<T>
      */
     public LinkedList<T> getOutputSet() {
         return outputSet;
     }
 
     /**
-     *
+     * Sets the output pattern set.
      * @param outputSet
      */
     public void setOutputSet(LinkedList<T> outputSet) {
@@ -87,9 +80,9 @@ public class Pattern<T extends Comparable<T>> {
     }
 
     /**
-     *
+     * Allows you to get a value from a specific index from the input pattern set.
      * @param index
-     * @return
+     * @return T
      */
     public T getInputPatternValue(int index)
     {
@@ -97,7 +90,7 @@ public class Pattern<T extends Comparable<T>> {
     }
 
     /**
-     *
+     * Allows you to set a value to the end of the input pattern set.
      * @param value
      */
     public void setInputPatternValue(T value)
@@ -106,9 +99,19 @@ public class Pattern<T extends Comparable<T>> {
     }
 
     /**
-     * Adds a value to the end of the input pattern list.
+     * Allows you to set a value at a specific index of the input pattern set.
+     * @param value
      * @param index
-     * @return
+     */
+    public void setInputPatternValueAtIndex(T value, int index)
+    {
+        this.getInputSet().add(index,value);
+    }
+
+    /**
+     * Allows you to get a value from a specific index from the output pattern set.
+     * @param index
+     * @return T
      */
     public T getOutputPatternValue(int index)
     {
@@ -116,11 +119,21 @@ public class Pattern<T extends Comparable<T>> {
     }
 
     /**
-     * Adds a value to the end of the output pattern list
+     * Allows you to set a value to the end of the output pattern set.
      * @param value
      */
     public void setOutputPatternValue(T value)
     {
         this.getOutputSet().add(value);
+    }
+
+    /**
+     * Allows you to set a value at a specific index of the output pattern set.
+     * @param value
+     * @param index
+     */
+    public void setOutputPatternValueAtIndex(T value, int index)
+    {
+        this.getOutputSet().add(index, value);
     }
 }
--- NetworksClasses/src/test/java/edu/mason/insf/PatternTest.java
+++ NetworksClasses/src/test/java/edu/mason/insf/PatternTest.java
@@ -1,38 +1,102 @@
 package edu.mason.insf;
+import edu.mason.insf.ann.utils.Constants;
+import edu.mason.insf.ann.utils.Helper;
 import edu.mason.insf.ann.utils.Pattern;
+import org.junit.Before;
 import org.junit.Test;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
+/**
+ * A class to test the Pattern Objects found in edu.mason.insf.ann.utils.Pattern
+ */
 public class PatternTest
 {
-    public Pattern<Integer> testPattern = new Pattern<Integer>();
+    Pattern<Double> testPattern;
 
-    @Test
-    public void testPatternId()
+    /**
+     *  The SetUp function that reads in data and initializes the testPattern object that will be tested.
+     */
+    @Before
+    public void SetUp()
     {
-        testPattern.setId(1);
-        assertEquals(1, testPattern.getId());
+        Helper helper = new Helper();
+        ArrayList<String> testData = helper.readFileToMemory(Constants.TEST_FILE);
+        ArrayList<Pattern<Double>> inputPatterns = helper.turnListToPattern(testData);
+        testPattern = inputPatterns.get(0);
     }
 
+    /**
+     * A test for the getInputSet function.
+     */
     @Test
     public void testInputSet()
     {
-        LinkedList<Integer> inputSet = new LinkedList<Integer>();
-        inputSet.add(1);
-
-        testPattern.setInputSet(inputSet);
-        assertEquals((int)1, (int)testPattern.getInputSet().get(0));
-
+        assertNotNull(testPattern);
+        assertEquals(0, testPattern.getInputSet().get(0),0);
     }
 
+    /**
+     * A test for the getOutputSet function.
+     */
     @Test
     public void testOutputSet()
     {
-        LinkedList<Integer> outputSet = new LinkedList<Integer>();
-        outputSet.add(2);
+        assertNotNull(testPattern);
+        assertEquals(-1, testPattern.getOutputSet().get(0),0);
+    }
 
-        testPattern.setOutputSet(outputSet);
-        assertEquals((int)2, (int)testPattern.getOutputSet().get(0));
+    /**
+     * A test for the getInputPatternSize function.
+     */
+    @Test
+    public void testGetInputSetSize()
+    {
+        assertEquals(3, testPattern.getInputPatternSize());
+    }
+
+    /**
+     * A test for the getOutputPatternSize function.
+     */
+    @Test
+    public void testGetOutputSetSize()
+    {
+        assertEquals(1, testPattern.getOutputPatternSize());
+    }
+
+    /**
+     * A test for the getInputPatternValue function.
+     */
+    @Test
+    public void testGetInputPatternValue()
+    {
+        assertEquals(0.8863739834514472,testPattern.getInputPatternValue(1),0);
+    }
+
+    /**
+     * Tests for the setInputPatterValue and setInputPatternValueAtIndex functions.
+     */
+    @Test
+    public void testSetInputPatternValue()
+    {
+        testPattern.setInputPatternValue(21.0);
+        assertEquals(21.0, testPattern.getInputPatternValue(testPattern.getInputPatternSize()-1),0);
+
+        testPattern.setInputPatternValueAtIndex(22.0,0);
+        assertEquals(22.0, testPattern.getInputPatternValue(0),0);
+    }
+
+    /**
+     * Tests for the setOutputPatternValue and setOutputPatternValueAtIndex functions.
+     */
+    @Test
+    public void testSetOutputPatternValue()
+    {
+        testPattern.setOutputPatternValue(31.0);
+        assertEquals(31.0, testPattern.getOutputPatternValue(testPattern.getOutputPatternSize()-1),0);
+
+        testPattern.setOutputPatternValueAtIndex(32.0,0);
+        assertEquals(32.0, testPattern.getOutputPatternValue(0),0);
     }
 }
