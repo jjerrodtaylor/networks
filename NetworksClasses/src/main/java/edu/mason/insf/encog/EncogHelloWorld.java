245821b8421924481a9558b0105b846875edfefb
jamaal.taylor <j.jerrod.taylor@gmail.com>
2013-11-18 17:12:27 -0300

    To run the hello world example for the Encog framework.
    http://www.heatonresearch.com/wiki/Hello_World

---
+++ NetworksClasses/src/main/java/edu/mason/insf/encog/EncogHelloWorld.java
@@ -0,0 +1,11 @@
+package edu.mason.insf.encog;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jamaaltaylor
+ * Date: 11/18/13
+ * Time: 1:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EncogHelloWorld {
+}
