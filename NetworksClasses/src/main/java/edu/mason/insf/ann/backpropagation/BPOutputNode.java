d0b51e0f33ef7866101a5583f97f2c890d01a196
jamaal.taylor <j.jerrod.taylor@gmail.com>
2013-11-20 00:47:22 -0300

    Learn some shit.

--- NetworksClasses/src/main/java/edu/mason/insf/ann/backpropagation/BPOutputNode.java
+++ NetworksClasses/src/main/java/edu/mason/insf/ann/backpropagation/BPOutputNode.java
@@ -20,11 +20,21 @@ public class BPOutputNode extends BPNode {
     }
 
     /*
-    * I need to read over the learning algorithm more
+    * I'm not sure if this is correct. I need to find out how to verify this.
     * */
     public void learn()
     {
+        double delta;
+        value.put(Constants.NODE_ERROR,this.computerError());
 
+        for(int i=0; i<inLinks.size(); i++)
+        {
+            delta = value.get(Constants.LEARNING_RATE) *
+                    value.get(Constants.NODE_ERROR) *
+                    inLinks.get(i).getValue(Constants.WEIGHT);
+
+            inLinks.get(i).updateWeight(delta);
+        }
     }
 
 
